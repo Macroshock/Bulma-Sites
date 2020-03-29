48e2b49d326c7679f36ffd921b146cade7883358
Macroshock <macroshock.games@gmail.com>
2020-03-28 23:42:47 -0300

    Initial commit and minimal Express server setup

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+// Set the static files folder
+app.use(express.static('static'))
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'src/index.html'))
+})
+
+app.listen(3000, function () {
+  console.log('Running on http://localhost:3000')
+})
+
+console.log(__dirname)
