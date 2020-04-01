9500eca554174a58e5f947d46e66f6b2085c498b
Macroshock <macroshock.games@gmail.com>
2020-03-31 23:11:47 -0300

    Finished blog page

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'src/index.html'))
 })
 
+app.get('/blog', function (req, res) {
+  res.sendFile(path.join(__dirname, 'src/blog.html'))
+})
+
 app.listen(3000, function () {
   console.log('Running on http://localhost:3000')
 })
